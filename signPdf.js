67511066b6ebea3f0b72d7d0f375eab1a2ef411c
Testtsts <ihsannur0610@gmail.com>
2022-11-02 14:29:34 +0700

    create signature

--- signPdf.js
+++ signPdf.js
@@ -1,23 +1,34 @@
 const keyPair = require('./keyPair');
 const fs = require('fs')
 const pdfParse = require('pdf-parse')
+const crypto = require('crypto')
 
 main()
 
-async function createSign(filePath,privateKey){
+async function getPdfContent(filePath){
+    let pdfExtract
     let readFileSync = fs.readFileSync(filePath)
     try {
-        let pdfExtract = await pdfParse(readFileSync)
+      pdfExtract = await pdfParse(readFileSync)
+    } catch (error) {
+      throw new Error(error)
     }
-    catch (err){
-        throw new Error(err)
-    }
+    return pdfExtract.text
+}
+
+async function createSign(filePath,privateKey){
+    let fileContent= await getPdfContent(filePath)
+    const sign = crypto.createSign('SHA256')
+    sign.update(fileContent)
+    sign.end()
+    const signature = sign.sign(privateKey).toString('hex')
+    console.log(fileContent,signature)
 }
 
 function main(){
     key = keyPair.getPrivateKey()
     if (!process.argv[2]){
-        createSign('./test.pdf',key)
+        createSign('./dummy.pdf',key)
     }
     else{
         createSign(process.argv[2],key)
